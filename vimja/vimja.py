0ce9540747c91e854a52644ddb26fb8b20df709d
Richard Killam <richard.killam@bluespurs.com>
2013-07-16 18:38:58 -0300

    added in test function to show how to get the text

--- vimja/vimja.py
+++ vimja/vimja.py
@@ -4,9 +4,17 @@ from ninja_ide.core import plugin
 
 
 class Vimja(plugin.Plugin):
+    def test(self, event):
+        tab = self.editor_s.get_actual_tab()
+        cursor = tab.textCursor()
+        cursor.beginEditBlock()
+        cursor.insertText('hello ninja_ide')
+        cursor.endEditBlock()
+
     def initialize(self):
         # Init your plugin
         self.editor_s = self.locator.get_service('editor')
+        self.editor_s.editorKeyPressEvent.connect(self.test)
 
     def finish(self):
         # Shutdown your plugin
