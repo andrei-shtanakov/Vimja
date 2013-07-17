4cab7da47bc34e04c5c901ccc5dcb17b55419876
richard.killam <richard.killam@gmail.com>
2013-07-17 10:24:23 -0300

    intercepted key event

--- vimja/vimja.py
+++ vimja/vimja.py
@@ -1,20 +1,54 @@
-# -*- coding: UTF-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from ninja_ide.core import plugin
+from PyQt4 import QtCore
+from PyQt4.QtCore import Qt, QEvent
+from functools import wraps
+from PyQt4.QtGui import QTextCursor
+
+
+class CEvent(QEvent):
+
+    def __init__(self):
+        super(CEvent, self).__init__()
+
+    def keyPressEvent(e):
+        for i in range(10):
+            pass
 
 
 class Vimja(plugin.Plugin):
-    def test(self, event):
-        tab = self.editor_s.get_actual_tab()
-        cursor = tab.textCursor()
-        cursor.beginEditBlock()
-        cursor.insertText('hello ninja_ide')
-        cursor.endEditBlock()
+
+    def interpretKeyBindings(self, function):
+        @wraps(function)
+        def _inner(event):
+            return function(event)
+            if True:
+                if True:
+                    event.ignore()
+                    return
+            return function(event)
+        return _inner
+        #tab = self.editor_s.get_actual_tab()
+        #cursor = tab.textCursor()
+        #cursor.beginEditBlock()
+
+        #cursor.insertText('hi')
+        #cursor.endEditBlock()
+        pass
 
     def initialize(self):
         # Init your plugin
+        self.__NORMAL_MODE = 0
+        self.__INSERT_MODE = 1
+
+        self.mode = self.__NORMAL_MODE
+
         self.editor_s = self.locator.get_service('editor')
-        self.editor_s.editorKeyPressEvent.connect(self.test)
+
+        editor = self.editor_s.get_editor()
+        editor.keyPressEvent = self.interpretKeyBindings(editor.keyPressEvent)
 
     def finish(self):
         # Shutdown your plugin
